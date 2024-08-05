e2f6cf990a2ad14d196499863e8ab13bb629c1c0
Matt Strong <mastro1@stanford.edu>
2024-08-04 18:24:19 -0700

    1. add monocular depth class

--- src/gaussian_splatting/gaussian_splatting_py/__init__.py
+++ src/gaussian_splatting/gaussian_splatting_py/__init__.py
@@ -1,4 +1,6 @@
 from .splatfacto2d import Splatfacto2d
 from .splatfacto3d import Splatfacto3D
 from .base_splatfacto import ROSSplatfacto
-from .vision_utils.vision_utils import convert_intrinsics, warp_image, compute_homography
+from .vision_utils.vision_utils import convert_intrinsics, warp_image, compute_homography, learn_scale_and_offset_raw, resize_if_too_large, preprocess_image
+from .monocular_depth import MonocularDepth
+from .load_yaml import load_config
