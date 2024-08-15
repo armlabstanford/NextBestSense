40714ceb4a23034013f2aff8a5a7162058e2e5fe
Matt Strong <mastro1@stanford.edu>
2024-08-15 16:01:49 -0700

    Add DT Camera Pack

---
+++ src/DenseTactNode/README.md
@@ -0,0 +1,13 @@
+# DenseTact Cam Node Package
+
+## INSTALLATION
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+roslaunch DenseTactNode dt.launch ckpt:=/path/to/model
+```
---
+++ src/DenseTactNode/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+package_name = 'DenseTact'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Boshu Lei',
+    maintainer_email='sobrmesa121@gmail.com',
+    description='Description of your ROS package',
+    license='License declaration',
+    tests_require=['pytest']
+)
