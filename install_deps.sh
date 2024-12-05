dd8c14a01b5af26003313a2b7bb2116f8d7917e9
Matt Strong <32246357+peasant98@users.noreply.github.com>
2024-12-04 16:40:44 -0800

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -4,6 +4,7 @@ PATH="${PATH}:/home/user/.local/bin"
 cd ..
 cd FisherRF-ns/
 
+git clone https://github.com/JiangWenPL/modified-diff-gaussian-rasterization-w-depth
 cd modified-diff-gaussian-rasterization-w-depth/
 pip install -e . -v
 cd ..
